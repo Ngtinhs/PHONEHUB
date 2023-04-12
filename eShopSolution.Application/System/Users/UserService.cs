3a98f31fec1adfd0610d9db74f89b0bbd4eab4b0
ngtinh <nguyenvanductinh77@gmail.com>
2023-04-12 21:58:15 +0700

    Add cookie authentication without identity

--- eShopSolution.Application/System/Users/UserService.cs
+++ eShopSolution.Application/System/Users/UserService.cs
@@ -18,18 +18,23 @@ namespace eShopSolution.Application.System.Users
         private readonly SignInManager<AppUser> _signInManager;
         private readonly RoleManager<AppRole> _roleManager;
         private readonly IConfiguration _config;
-        public UserService(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<AppRole> roleManager, IConfiguration config)
+
+        public UserService(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,
+            RoleManager<AppRole> roleManager, IConfiguration config)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _roleManager = roleManager;
             _config = config;
         }
+
+        // Phương thức Login
         public async Task<string> Authencate(LoginRequest request)
         {
             var user = await _userManager.FindByNameAsync(request.UserName);
             if (user == null) return null;
 
+            // Tham số cuối là bool : true thì kích hoạt isPersistent ( chức năng login fail nhiều lần thì lock )
             var result = await _signInManager.PasswordSignInAsync(user, request.Password, request.RememberMe, true);
             if (!result.Succeeded)
             {
@@ -40,20 +45,25 @@ namespace eShopSolution.Application.System.Users
             {
                 new Claim(ClaimTypes.Email,user.Email),
                 new Claim(ClaimTypes.GivenName,user.FirstName),
-                new Claim(ClaimTypes.Role,string.Join(";",role))
+                new Claim(ClaimTypes.Role,string.Join(";",role)),
+                new Claim(ClaimTypes.Name, request.UserName)
             };
+
+            // Sau khi có được claim thì ta cần mã hóa nó
+            // Tokens key và issuer truy cập được thông qua DI 1 Iconfig
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
+            // Cần cài JWT
             var token = new JwtSecurityToken(_config["Tokens:Issuer"],
                 _config["Tokens:Issuer"],
                 claims,
                 expires: DateTime.Now.AddHours(3),
                 signingCredentials: creds);
 
+            // Khi thành công thì ta sẽ trả về 1 cái string token
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
-
 
         public async Task<bool> Register(RegisterRequest request)
         {
@@ -66,6 +76,8 @@ namespace eShopSolution.Application.System.Users
                 UserName = request.UserName,
                 PhoneNumber = request.PhoneNumber
             };
+
+            // Mặc định Password phải có ít nhất 1 lower case và 1 upper case
             var result = await _userManager.CreateAsync(user, request.Password);
             if (result.Succeeded)
             {
--- eShopSolutionBackendApi/Controllers/UsersController.cs
+++ eShopSolutionBackendApi/Controllers/UsersController.cs
@@ -32,7 +32,7 @@ namespace eShopSolutionBackendApi.Controllers
             {
                 return BadRequest("User name or Password is incorrect");
             }
-            return Ok(new { token = resultToken });
+            return Ok(resultToken);
         }
 
         [HttpPost("register")]
