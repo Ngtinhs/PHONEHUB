e8e2c9b709c3ff034303557ff432fb544dac32e1
ngtinh <nguyenvanductinh77@gmail.com>
2023-04-25 11:00:05 +0700

    update

--- EmailService/EmailService.cs
+++ EmailService/EmailService.cs
@@ -33,7 +33,7 @@ namespace EmailService
             // send email
             using var smtp = new SmtpClient();
             smtp.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
-            smtp.Authenticate("suppport.phonehub@gmail.com", "asiwcynzfajzrdxr");
+            smtp.Authenticate("contact.phonehub.vn@gmail.com", "asiwcynzfajzrdxr");
 
             try
             {
